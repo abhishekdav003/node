53497f434cc77b27853c143ff71c18e1ba4853a4
abhishekdav003 <abhishekdav003@gmail.com>
2026-06-24 07:20:03 +0530

    hello done

--- app.js
+++ app.js
@@ -1,22 +1,44 @@
-const http = require("http")
+const http = require("http");
+const fs = require("fs");
+
 const server = http.createServer((req, res) => {
-  const url = req.url
-  const header = req.header
-  if (req.url === "/") {
-    res.setHeader("Content-Type", "text/html")
-  
-    res.end(
-      `
-    <form>
-    <label>Name:</label>
-    <input type="text" name="username"></input>
-    <button type="submit">Add</button>
-    </form>
-    `
-    )
+  if (req.url === "/" && req.method === "GET") {
+    res.setHeader("Content-Type", "text/html");
+
+    res.end(`
+      <form action="/message" method="POST">
+        <label>Name:</label>
+        <input type="text" name="username">
+        <button type="submit">Add</button>
+      </form>
+    `);
+  } else if (req.url === "/message" && req.method === "POST") {
+    const body = [];
+
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      const buffer = Buffer.concat(body);
+      const formData = buffer.toString();
+
+      const username = formData.split("=")[1];
+
+      fs.writeFile("message.txt", username, (err) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        res.end();
+      });
+    });
   }
-})
+});
 
 server.listen(3000, () => {
-  console.log("server is running on port 3000")
-})
+  console.log("Server is running on port 3000");
+});
